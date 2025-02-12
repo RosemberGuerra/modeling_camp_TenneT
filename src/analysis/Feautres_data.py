8f6879c6c5fd01f6e04daff7cb011a325d122d00
Rosember Guerra Urzola <Rosember.guerra@gmail.com>
2025-02-12 11:13:55 +0100

    analysis feature data

---
+++ src/analysis/Feautres_data.py
@@ -0,0 +1,10 @@
+# Analysis using the features_output_test.csv fle
+
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+import src.year_selection.climate_year_selection as cys
+
+# Load the features_output_test.csv file
+features_data = pd.read_csv("data/features_output_test.csv")
+
