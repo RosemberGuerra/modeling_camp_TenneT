3d693e37a0a181ec77aeac90b7ec3999c75dc8be
Joris Kersten <49530289+joriskersten@users.noreply.github.com>
2025-02-13 15:37:53 +0100

    PlotHistogram.py cleanup.

--- code/kmeans/PlotHistogram.py
+++ code/kmeans/PlotHistogram.py
@@ -1,25 +1,28 @@
 from pathlib import Path
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-n_clusters = 6
-print(f"Making plots for {n_clusters} clusters")
-input_file = Path(f"../../data/kmeans_result/cyfeatures_TA_{n_clusters}clusters.csv")
+# Settings
+n_clusters = 4   # Determines the data set used.
+column_number = 8   # This column will be used for the histogram
+use_density = False   # Normalization is used if use_density is True
+bins = 16
+
+# Select the input file 
+print(f"Making plots for {n_clusters} clusters\n")
+input_file = Path(f"../../data/kmeans_result/cyfeatures_{n_clusters}clusters_new.csv")
 
 # Load data
 df = pd.read_csv(input_file)
 
-# Print column numbers and names
+# Print column numbers and names and print selected column
 for idx, col in enumerate(df.columns):
     print(f"Column {idx}: {col}")
 print()
+print(f"Column selected: {column_number}: {df.columns[column_number]}\n")
+# print(df.iloc[:, column_number])
 
-column_number = 2  # Ensure this column contains numerical data
-print(f"Column selected: {column_number}: {df.columns[column_number]}")
-print(df.iloc[:, column_number])
-
-# Ensure 'selected' column exists and is boolean
+# Column 'selected' should exist and contain bools
 if 'selected' in df.columns:
     df['selected'] = df['selected'].astype(bool)
 else:
@@ -29,20 +32,21 @@ else:
 all_values = df.iloc[:, column_number]
 selected_values = df[df['selected']].iloc[:, column_number]
 
-# Create histograms
+# Create the plot
 plt.figure(figsize=(8, 6))
 
-# Define bins
-bins = 16  # Adjust as needed
-
-# Plot histograms using density=True for normalization
-plt.hist(all_values, bins=bins, alpha=0.5, label=df.columns[column_number], color='blue', density=True)
-plt.hist(selected_values, bins=bins, alpha=0.5, label='Selected Data', color='red', density=True)
+# Plot histograms
+plt.hist(all_values, bins=bins, alpha=0.5, label=df.columns[column_number], color='blue', density=use_density)
+plt.hist(selected_values, bins=bins, alpha=0.5, label='Selected Data', color='red', density=use_density)
 
 # Labels and legend
 plt.xlabel(df.columns[column_number])
-plt.ylabel('Density')
-plt.title(f'Histogram of {df.columns[column_number]} (Normalized)')
+if not use_density:
+    plt.ylabel('Frequency')
+    plt.title(f'Histogram of {df.columns[column_number]}')
+else:
+    plt.ylabel('Density')
+    plt.title(f'Histogram of {df.columns[column_number]} (Normalized)')
 plt.legend()
 
 # Show plot
